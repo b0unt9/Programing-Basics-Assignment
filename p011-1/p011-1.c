363b33e1c3ef304fc71084e481c74533d834cb9c
SH <40918958+jmt4k8awngdw1rki3pln@users.noreply.github.com>
2020-09-15 14:34:56 +0900

    p011-1

---
+++ p011-1/p011-1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+int main()
+{
+	int sec, min, hour, day;
+	scanf("%d",&sec);
+	
+	min=sec/60;
+	hour=min/60;
+	day=hour/24;
+	sec=sec%60;
+	min=min%60;
+	hour=hour%24;
+	
+	printf("%d %d %d %d", day, hour, min, sec);
+}
