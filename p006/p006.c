6933109df3dfbf30006b84cf11650cf60bfc569b
SH <40918958+jmt4k8awngdw1rki3pln@users.noreply.github.com>
2020-09-15 14:34:19 +0900

    p006

---
+++ p006/p006.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+int main() {
+	long long int a;
+	long long int b;
+	
+	scanf("%lld", &a);
+	scanf("%lld", &b);
+	printf("%lld", a+b);
+	
+	return 0;
+}
