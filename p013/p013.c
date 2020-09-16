d49f472c461d3895d7ca104f7700d67695913bbb
SH <40918958+jmt4k8awngdw1rki3pln@users.noreply.github.com>
2020-09-16 15:26:00 +0900

    p013

---
+++ p013/p013.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+int main()
+{
+	double a, b, c, d, e, f;
+	scanf("%d %d %d %d", &a, &b, &c, &d);
+	e = a+b+c+d;
+	f = d/e*100;
+	printf("%.2f\%%", f);
+	return 0;
+}
